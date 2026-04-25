fd9daa59b5111f185e0a154faa89d1e5535b8a8c
Mohamad Rizal Nurochman <muhamadrizalnurohman072005@gmail.com>
2026-04-25 09:54:21 +0700

    Feat: implement pagination for designer

--- modules/designer/controller/designer_controller.go
+++ modules/designer/controller/designer_controller.go
@@ -3,15 +3,21 @@ package controller
 import (
 	"net/http"
 
+	"github.com/Caknoooo/go-pagination"
 	"github.com/Rizal-Nurochman/matchnbuild/modules/designer/dto"
+	"github.com/Rizal-Nurochman/matchnbuild/modules/designer/query"
 	"github.com/Rizal-Nurochman/matchnbuild/modules/designer/service"
+	"github.com/Rizal-Nurochman/matchnbuild/pkg/constants"
 	"github.com/Rizal-Nurochman/matchnbuild/pkg/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/samber/do"
+	"gorm.io/gorm"
 )
 
 type (
 	designerProfileHandler struct {
 		designerService service.DesignerService
+		db *gorm.DB
 	}
 
 	DesignerProfileHandler interface {
@@ -22,19 +28,26 @@ type (
 	}
 )
 
-func NewDesignerProfileHandler(designerService service.DesignerService) DesignerProfileHandler  {
-	return &designerProfileHandler{designerService: designerService}
+func NewDesignerProfileHandler(injector *do.Injector, designerService service.DesignerService) DesignerProfileHandler  {
+	db := do.MustInvokeNamed[*gorm.DB](injector, constants.DB)
+	return &designerProfileHandler{designerService: designerService, db: db}
 }
 
 func (h *designerProfileHandler) GetAll(ctx *gin.Context)  {
-	designers, err := h.designerService.GetAll(ctx.Request.Context())
+	var filter = &query.DesignerFilter{}
+	filter.BindPagination(ctx)
+
+	ctx.ShouldBindQuery(filter)
+
+	designers, total, err := pagination.PaginatedQueryWithIncludable[query.Designer](h.db, filter)
 	if err != nil {
-		res := utils.BuildResponseFailed(dto.MESSAGE_FAILED_GET_DESIGNER_PROFILE, err.Error(), nil)
+		res:=utils.BuildResponseFailed(dto.ErrDesignerProfileNotFound.Error(), err.Error(), nil)
 		ctx.JSON(http.StatusBadRequest, res)
 		return
 	}
 
-	res := utils.BuildResponseSuccess(dto.MESSAGE_SUCCESS_GET_DESIGNER_PROFILE, designers)
+	paginationRes := pagination.CalculatePagination(filter.Pagination, total)
+	res := pagination.NewPaginatedResponse(http.StatusOK, dto.MESSAGE_SUCCESS_GET_DESIGNER_PROFILE, designers, paginationRes)
 	ctx.JSON(http.StatusOK, res)
 }
 
@@ -53,9 +66,27 @@ func (h *designerProfileHandler) GetByID(ctx *gin.Context)  {
 }
 
 func (h *designerProfileHandler) GetMyProfile(ctx *gin.Context)  {
-	
+	userID := ctx.MustGet("user_id").(string)
+
+	designer, err := h.designerService.GetMyProfile(ctx.Request.Context(), userID)
+	if err != nil {
+		res := utils.BuildResponseFailed(dto.MESSAGE_FAILED_GET_DESIGNER_PROFILE, err.Error(), nil)
+		ctx.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+	res := utils.BuildResponseSuccess(dto.MESSAGE_SUCCESS_GET_DESIGNER_PROFILE, designer)
+	ctx.JSON(http.StatusOK, res)
 }
 
 func (h *designerProfileHandler) Update(ctx *gin.Context)  {
-	
+	var req dto.DesignerProfileUpdateRequest
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		res := utils.BuildResponseFailed(dto.MESSAGE_FAILED_UPDATE_DESIGNER_PROFILE, err.Error(), nil)
+		ctx.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+
 }
---
+++ modules/designer/query/designer_query.go
@@ -0,0 +1,62 @@
+package query
+
+import (
+	"github.com/Caknoooo/go-pagination"
+	"gorm.io/gorm"
+)
+
+type Designer struct {
+	ID              string  `json:"id"`
+	UserID          string  `json:"user_id"`
+	Name            string  `json:"name"`
+	ProfilePicture  *string `json:"profile_picture"`
+	Bio             string  `json:"bio"`
+	ExperienceYears int     `json:"experience_years"`
+	IsVerified      bool    `json:"is_verified"`
+	IsAvailable     bool    `json:"is_available"`
+	Location        string  `json:"location"`
+}
+
+type DesignerFilter struct {
+	pagination.BaseFilter
+}
+
+func (f *DesignerFilter) ApplyFilters(query *gorm.DB) *gorm.DB {
+	query = query.Joins("JOIN users ON users.id = designer_profiles.users_id").Select("designer_profiles.*, users.name, users.profile_picture")
+	return query
+}
+
+func (f *DesignerFilter) GetTableName() string {
+	return "designer_profiles"
+}
+
+func (f *DesignerFilter) GetSearchFields() []string {
+	return []string{"name"}
+}
+
+func (f *DesignerFilter) GetDefaultSort() string {
+	return "id asc"
+}
+
+func (f *DesignerFilter) GetIncludes() []string {
+	return f.Includes
+}
+
+func (f *DesignerFilter) GetPagination() pagination.PaginationRequest {
+	return f.Pagination
+}
+
+func (f *DesignerFilter) Validate() {
+	var validIncludes []string
+	allowedIncludes := f.GetAllowedIncludes()
+	for _, include := range f.Includes {
+		if allowedIncludes[include] {
+			validIncludes = append(validIncludes, include)
+		}
+	}
+	f.Includes = validIncludes
+}
+
+func (f *DesignerFilter) GetAllowedIncludes() map[string]bool {
+	return map[string]bool{}
+}
