06f52b1715aac4509472796a213364db39462e3d
vitoramadhan <vito@itpi.co.id>
2026-06-04 11:30:20 +0700

    fix imagekit

--- pkg/utils/imagekit.go
+++ pkg/utils/imagekit.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"io"
+	"log"
+	"mime"
 	"mime/multipart"
 	"os"
 	"path/filepath"
@@ -12,6 +16,15 @@ import (
 	"github.com/imagekit-developer/imagekit-go/v2/option"
 	"github.com/imagekit-developer/imagekit-go/v2/packages/param"
 )
+
+type namedReader struct {
+	*bytes.Reader
+	filename    string
+	contentType string
+}
+
+func (n *namedReader) Filename() string    { return n.filename }
+func (n *namedReader) ContentType() string { return n.contentType }
 
 var (
 	ErrFileTooLarge       = errors.New("file size exceeds maximum limit of 5MB")
@@ -42,19 +55,48 @@ func UploadToImageKit(file multipart.File, header *multipart.FileHeader, folder 
 		return "", ErrFileTypeNotAllowed
 	}
 
+	buf, err := io.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+
+	contentType := header.Header.Get("Content-Type")
+	if contentType == "" {
+		contentType = mime.TypeByExtension(ext)
+	}
+	if contentType == "" {
+		contentType = "application/octet-stream"
+	}
+
+	reader := &namedReader{
+		Reader:      bytes.NewReader(buf),
+		filename:    header.Filename,
+		contentType: contentType,
+	}
+
 	privateKey := os.Getenv("IMAGEKIT_PRIVATE_KEY")
+	if privateKey == "" {
+		return "", errors.New("IMAGEKIT_PRIVATE_KEY is not set")
+	}
 
 	client := imagekit.NewClient(
 		option.WithPrivateKey(privateKey),
 	)
 
 	resp, err := client.Files.Upload(context.Background(), imagekit.FileUploadParams{
-		File:     file,
-		FileName: header.Filename,
-		Folder:   param.NewOpt(folder),
+		File:              reader,
+		FileName:          header.Filename,
+		Folder:            param.NewOpt(folder),
+		UseUniqueFileName: param.NewOpt(true),
 	})
 	if err != nil {
+		log.Printf("[imagekit] upload error file=%q folder=%q: %v", header.Filename, folder, err)
 		return "", err
+	}
+
+	if resp.URL == "" {
+		log.Printf("[imagekit] upload succeeded but URL is empty, raw=%s", resp.RawJSON())
+		return "", errors.New("imagekit returned empty URL")
 	}
 
 	return resp.URL, nil
